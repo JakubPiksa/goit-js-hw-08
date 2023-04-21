5e85045577ecd8863484a3b8c53cd198a2c11776
Jakub Piksa <kuba.piksa@wp.pl>
2023-04-21 23:43:28 +0200

    i think finally its gonna work

---
+++ src/js/01-gallery.js
@@ -0,0 +1,31 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = '';
+for (const image of galleryItems) {
+  const newImage = `<a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>`;
+  galleryEl.innerHTML += newImage;
+}
+
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+  const instance = basicLightbox.create(`<img src="${event.target.dataset.source}"/>`, {
+    onShow: instance => {
+      window.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+          instance.close();
+        }
+      });
+    },
+  });
+  instance.show();
+});
