98d569937ee71778d0f44aed1553cd8559aa6a6c
Jakub Piksa <kuba.piksa@wp.pl>
2023-04-23 15:41:48 +0200

    gettin there

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+const player = new Vimeo.Player(iframe);
 
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+
+player.on('play', onPlay);
