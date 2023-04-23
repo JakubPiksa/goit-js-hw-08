ab17544b212357cd1a94c1a786ff007cc1b59184
Jakub Piksa <kuba.piksa@wp.pl>
2023-04-23 21:24:43 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,11 +7,26 @@ const player = new Player(iframe);
 const saveTime = data => localStorage.setItem('videoplayer-current-time', data.seconds);
 const saveTimeDelay = throttle(saveTime, 1000);
 
-player.on('timeupdate', function (data) {
+
+player.on('timeupdate', function(data) {
     const currentTime = data.seconds;
     saveTimeDelay(data);
 });
 
+const getCurrentTime = localStorage.getItem('videoplayer-current-time');
+if (getCurrentTime) {
+    player.setCurrentTime(getCurrentTime).then(function (seconds) {
+       
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+              
+                break;
 
+            default:
+                
+                break;
+        }
+    });
 
-
+}
